8cb0fcc3898da47372a9dc3c8cb86f6032882f32
Shadow Looters <108761193+SVerseLab@users.noreply.github.com>
2022-09-14 17:23:27 +0100

    Network Logo update

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -37,8 +37,8 @@ export const connectorsByName = {
   [ConnectorNames.WalletLink]: async () => {
     const { WalletLinkConnector } = await import('@web3-react/walletlink-connector')
     return new WalletLinkConnector({
-      url: 'https://pancakeswap.finance',
-      appName: 'PancakeSwap',
+      url: 'https://shadowswap.xyz',
+      appName: 'ShadowSwap',
       appLogoUrl: 'https://pancakeswap.com/logo.png',
       supportedChainIds: SUPPORTED_CHAIN_ID,
     })
