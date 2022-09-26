37de680522b4af5c7eb7dac54840ab04ff45e0f2
Shadow Looters <108761193+SVerseLab@users.noreply.github.com>
2022-09-26 20:38:36 +0100

    wallet modal

--- packages/uikit/src/widgets/WalletModal/config.tsx
+++ packages/uikit/src/widgets/WalletModal/config.tsx
@@ -16,7 +16,7 @@ const connectors: Config[] = [
     icon: Metamask,
     connectorId: ConnectorNames.Injected,
     priority: 1,
-    href: "https://metamask.app.link/dapp/pancakeswap.finance/",
+    href: "https://metamask.app.link/dapp/shadowswap.xyz/",
   },
   {
     title: "Coinbase Wallet",
