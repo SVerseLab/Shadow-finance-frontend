f2216b46ae051c4a8e457edcd7d594664a36e51f
Samuel Ige <59492619+IoneGod@users.noreply.github.com>
2022-10-04 08:52:04 +0100

    Fix Social.items in Social Links.tsx

--- packages/uikit/src/components/Footer/Components/SocialLinks.tsx
+++ packages/uikit/src/components/Footer/Components/SocialLinks.tsx
@@ -16,9 +16,11 @@ const SocialLinks: React.FC<FlexProps> = ({ ...props }) => (
       };
       const Icon = social.icon;
       const mr = index < socials.length - 1 ? "24px" : 0;
+      //@ts-ignore
       if (social.items) {
         return (
           <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} />}>
+            //@ts-ignore
             {social.items.map((item) => (
               <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
                 {item.label}
